659e945d01ccd95ce31e509c643cbdd4c1421d5a
Lily Zhang <lily.zhang@tufts.edu>
2019-07-19 16:36:33 -0400

    edited script to pull all Thingworx properties, cleaned up visualizations

---
+++ ARBackpack/Assets/StabilizeObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StabilizeObject : MonoBehaviour
+{
+    public GameObject AR_panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("starting stabilizer code");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AR_panel.transform.LookAt(Camera.main.transform.position, Vector3.up);
+
+    }
+
+}
---
+++ ARBackpack/Assets/helloPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloPanel : MonoBehaviour
+{
+    public GameObject thisPanel;
+    bool state;
+
+    void Start()
+    {
+        state = true;
+    }
+    public void showPanel()
+    {
+        state = !state;
+        thisPanel.SetActive(state);
+    }
+}
